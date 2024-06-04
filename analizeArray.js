1ec721ddd1ca02ec568db64cba84d5a2fc4617a1
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-03 23:35:40 -0500

    Limpia el codigo

--- analizeArray.js
+++ analizeArray.js
@@ -1,29 +1,14 @@
 export const analizeArray = (array) => {
     if (!array) return undefined;
-    // const average =
-    //     array.reduce((sum, current) => sum + current, 0) / array.length;
-    // const max = array.reduce(
-    //     (mem, current) => (mem > current ? mem : current),
-    //     -Infinity,
-    // );
-    // const min = array.reduce(
-    //     (mem, current) => (mem < current ? mem : current),
-    //     Infinity,
-    // );
-    // const analizedArray = {
-    //     average: !array.length ? null : average,
-    //     min: !array.length ? null : min,
-    //     max: !array.length ? null : max,
-    //     length: array.length,
-    // };
 
-    const analized = array.reduce(
+    return array.reduce(
         (mem, curr, index) => {
             mem.average = !mem.average ? curr : mem.average + curr;
             mem.min = !mem.min ? curr : mem.min < curr ? mem.min : curr;
             mem.max = !mem.max ? curr : mem.max > curr ? mem.max : curr;
 
-            if (index === mem.length - 1 ) mem.average = mem.average / mem.length
+            if (index === mem.length - 1)
+                mem.average = mem.average / mem.length;
 
             return mem;
         },
@@ -34,6 +19,4 @@ export const analizeArray = (array) => {
             length: array.length,
         },
     );
-
-    return analized;
 };
