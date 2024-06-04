d0cab440cc582cd35aa31d824a5aaeab70e46a4f
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-04 10:20:36 -0500

    Renamed Vars for readability in cesarCipher.js

--- cesarCipher.js
+++ cesarCipher.js
@@ -1,12 +1,11 @@
 const shiftCharacter = (char, shift = 0) => {
     if (!/[a-zA-Z]/.test(char)) return char;
 
-    const lettersInAlphabet = 26;
-    const baseLetter = /[a-z]/.test(char) ? 97 : 65;
-    const newCharValue =
-        (char.charCodeAt(0) - baseLetter + shift) % lettersInAlphabet + baseLetter;
+    const totalLetters = 26;
+    const baseASCII = /[a-z]/.test(char) ? 97 : 65;
+    const newChar = (char.charCodeAt(0) - baseASCII + shift) % totalLetters + baseASCII;
 
-    return String.fromCharCode(newCharValue);
+    return String.fromCharCode(newChar);
 };
 
 export const cesarCypher = (str, shift) => {
