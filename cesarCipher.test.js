6ddc0629a6d82ad728d6d6bbd282be4b9b7383d3
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-03 21:03:05 -0500

    Crea el test para cesarCipher

--- cesarCipher.test.js
+++ cesarCipher.test.js
@@ -1,3 +1,17 @@
 import { cesarCypher } from "./cesarCipher";
 
+test('Cambia las letras en cualquier factor', () => {
+    expect(cesarCypher('abc',3)).toBe('def')
+})
 
+test('Mantiene las mayúsculas', () => {
+    expect(cesarCypher('HeLLo',3)).toBe('KhOOr')
+})
+
+test('Cuando llega al final del abecedario continua el conteo desde el principio', () => {
+    expect(cesarCypher('xYz',3)).toBe('aBc')
+})
+
+test('Sólo cambia las letras', () => {
+    expect(cesarCypher('Hola! Tonto.',1)).toBe('Ipmb! Upoup.')
+})
