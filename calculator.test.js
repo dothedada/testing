193fcaf1b9bcbba77b820f17dc964590210f74b1
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-03 20:06:16 -0500

    Limpia el test

--- calculator.test.js
+++ calculator.test.js
@@ -16,12 +16,8 @@ test('Realiza la división de dos numeros', () => {
     expect(calculator.divide(2, 5)).toBeCloseTo(0.4);
 });
 
-test('Retorna siempre un número que corresponde a la función, así el argumento ingresado sea una cadena', () => {
+test('Convierte las cadenas de números en números', () => {
     for (const operation in calculator) {
         expect(typeof calculator[operation]('3', '4')).toBe('number');
     }
-    expect(calculator.add('23', '7')).toBe(30);
-    expect(calculator.substract('5', '2')).toBe(3);
-    expect(calculator.multiply('4', '3')).toBe(12);
-    expect(calculator.divide('10', '2')).toBeCloseTo(5);
 });
