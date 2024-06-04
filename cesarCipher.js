48178115977ce3b4090c57be4b481338125d8112
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-03 21:43:47 -0500

    Integra el método de cesarCipher que pasa el test

--- cesarCipher.js
+++ cesarCipher.js
@@ -1,3 +1,17 @@
-export const cesarCypher = str => {
+const shiftCharacter = (char, shift = 0) => {
+    if (!/[a-zA-Z]/.test(char)) return char;
+
+    const lettersInAlphabet = 26;
+    const baseLetter = /[a-z]/.test(char) ? 97 : 65;
+    const newCharValue =
+        (char.charCodeAt(0) - baseLetter + shift) % lettersInAlphabet + baseLetter;
+
+    return String.fromCharCode(newCharValue);
+};
+
+export const cesarCypher = (str, shift) => {
     return str
-}
+        .split('')
+        .map((char) => shiftCharacter(char, shift))
+        .join('');
+};
