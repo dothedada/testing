02aae2c1a04c16b6ba0957dc39f4549da1f8cabb
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-03 21:07:50 -0500

    Refina el test cesarCipher

--- cesarCipher.test.js
+++ cesarCipher.test.js
@@ -4,6 +4,14 @@ test('Cambia las letras en cualquier factor', () => {
     expect(cesarCypher('abc',3)).toBe('def')
 })
 
+test('Retorna null cuando la cadena está vacía', () => {
+    expect(cesarCypher('', 3)).toBe('')
+})
+
+test('retorna la misma cadena si no se le introduce el segundo parámetro', () => {
+    expect(cesarCypher('abc')).toBe('abc')
+})
+
 test('Mantiene las mayúsculas', () => {
     expect(cesarCypher('HeLLo',3)).toBe('KhOOr')
 })
