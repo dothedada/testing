d146a88dff335547684624d326083d30b513cf89
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-04 11:03:41 -0500

    Experimenta con los métodos min y max de Math para simplificar el código de analizeArray.js

--- analizeArray.js
+++ analizeArray.js
@@ -6,16 +6,36 @@ export const analizeArray = (array) => {
             mem.average = index !== mem.length - 1 
                 ? (mem.average || 0 ) + curr 
                 : (mem.average + curr) / mem.length;
-            mem.min = Math.min( mem.min || curr, curr );
-            mem.max = Math.max( mem.max || curr, curr );
-
+            
             return mem;
         },
         {
             average: null,
-            min: null,
-            max: null,
+            min: !array.length ? null : Math.min(...array),
+            max: !array.length ? null : Math.max(...array),
             length: array.length,
         },
     );
 };
+
+// export const analizeArray = (array) => {
+//    if (!array) return undefined;
+//
+//    return array.reduce(
+//        (mem, curr, index) => {
+//            mem.average = index !== mem.length - 1 
+//                ? (mem.average || 0 ) + curr 
+//                : (mem.average + curr) / mem.length;
+//            mem.min = Math.min( mem.min || curr, curr );
+//            mem.max = Math.max( mem.max || curr, curr );
+//
+//            return mem;
+//        },
+//        {
+//            average: null,
+//            min: null,
+//            max: null,
+//            length: array.length,
+//        },
+//    );
+//};
