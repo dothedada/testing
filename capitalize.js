aceceebbae5f6c4c8c170c52eeee7c2c5f61d4c5
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-03 19:11:16 -0500

    Incorpora la funcion capitalize que paso todas los test

--- capitalize.js
+++ capitalize.js
@@ -1,3 +1,9 @@
-export const capitalize = args => {
-	return args
-}
+export const capitalize = (arg) => {
+    if (!arg) return null;
+    return arg
+        .split(' ')
+        .map((str) => {
+            return `${str[0].toUpperCase()}${str.slice(1)}`;
+        })
+        .join(' ');
+};
