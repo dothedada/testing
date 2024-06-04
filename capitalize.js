ebf84cc4cbd187eb632e1f42545fdf12b9928c1d
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-04 10:44:43 -0500

    Simplifica código de capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -2,8 +2,6 @@ export const capitalize = (arg) => {
     if (!arg) return null;
     return arg
         .split(' ')
-        .map((str) => {
-            return `${str[0].toUpperCase()}${str.slice(1)}`;
-        })
+        .map((str) => `${str[0].toUpperCase()}${str.slice(1)}`);
         .join(' ');
 };
