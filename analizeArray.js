e7d66f4fef960325ad71fe45a1661d80c0f3c4d4
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-03 22:48:45 -0500

    Primera iteración de la solución de los tests

--- analizeArray.js
+++ analizeArray.js
@@ -1,3 +1,13 @@
-export const analizeArray = array => {
-    //
+export const analizeArray = (array) => {
+    if (!array) return undefined
+    const average = array.reduce((sum, current) => sum + current, 0) / array.length
+    const max = array.reduce((mem, current) => mem > current ? mem : current, -Infinity)
+    const min = array.reduce((mem, current) => mem < current ? mem : current, Infinity)
+    const analizedArray = {
+        average: !array.length ? null : average, 
+        min: !array.length ? null : min, 
+        max: !array.length ? null : max, 
+        length: array.length
+    }
+    return analizedArray
 }
